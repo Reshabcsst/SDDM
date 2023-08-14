0367d17c0cd2da1e9c94caf97a6cbf151ba8854e
Reshabcsst <reshabn.csst@gmail.com>
2023-08-14 18:19:11 +0530

    uno

--- src/Components/Adds.js
+++ src/Components/Adds.js
@@ -175,7 +175,7 @@ const Adds = () => {
                                                 </g>
                                             </svg>
                                             ward no { }
-                                            {data.wordNo}
+                                            {data.wardNo}
                                         </p>
                                         <p>
                                             <svg width="18" height="23" viewBox="0 0 18 23" fill="none" xmlns="http://www.w3.org/2000/svg">
