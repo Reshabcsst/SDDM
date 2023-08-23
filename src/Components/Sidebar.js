7dbd5d5332c35f360c6abc18513dd47779c58781
Reshabcsst <reshabn.csst@gmail.com>
2023-08-23 18:27:30 +0530

    uno

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -7,7 +7,7 @@ const Sidebar = () => {
     return (
         <div className='main'>
             <div className="li">
-                <li><button onClick={() => navigate("/addward")}>Ward</button></li>
+                <li><button onClick={() => navigate("/addward")}>Ward List</button></li>
                 <li><button onClick={() => navigate("/assign")}>Assign Ward</button></li>
                 <li><button onClick={() => navigate("/registerAgancy")}>Register Agancy</button></li>
                 <li><button onClick={() => navigate("/recentPosts")}>Recent Posts</button></li>
